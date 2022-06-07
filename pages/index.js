9e59a4de95a38dc90853ab1fc521b727789135cf
charlesgobina <charles.gobina@gmail.com>
2022-06-08 00:35:53 +0100

    Add styling

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,16 @@ import Image from 'next/image'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div>
-      <Navbar/>
       <h1>Homepage</h1>
       <p>Hello, this is the homepage</p>
-      <Footer/>
+      <Link href="/ninjas">
+        <a>See Ninja Listing</a>
+      </Link>
     </div>
   )
 }
